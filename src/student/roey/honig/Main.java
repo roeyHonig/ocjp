db7fa9df477dedd470191c994f051a01ae0297e9
Roey Honig <roeyhonig94@gmail.com>
2018-04-29 14:07:41 +0300

    compile and run in terminal including arguments

--- src/student/roey/honig/Main.java
+++ src/student/roey/honig/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello, " + args[0]);
+
+    //  To compile and run this program from the terminal:
+    //  From within   ~/IdeaProjects/ocjp/src $      Location in Hard Disk where the source code of this program
+    // Type the following: javac student/roey/honig/Main.java   // will compile and create the file Main.class
+    // Type the following to Run:
+    // java student/roey/honig/Main roey          or:
+    // java student/roey/honig/Main "roey honig"
+    // see what happends
+
     }
 }
